f778bc8ff210fd63679a7488941c62084d1ec15d
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-02-07 12:01:52 +0530

    updt: new user register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,40 +1,47 @@
-import { User } from "../models/userModel.js"
+import { User } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 
 // New Registration of User
-export const register = async (req, res)=>{
-    try {
-        const { fullName, username, password, confirmPassword, gender } = req.body;
+export const register = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
 
-        if( fullName || !username || !password || !confirmPassword || !gender ) {
-            return res.status(400).json({message: "All fields are required."})
-        }
-
-        if( password !== confirmPassword) {
-            return res.status(400).json({message: "Passwords do not match."})
-        }
-
-        const user = await User.findOne({username});
-        if(user) {
-            return res.status(400).json({message: "Username already exist. Try different!"})
-        }
-
-        // Password hashed to maintain security.
-        const hashedPassword = await bcrypt.hash(password, 10); 
-
-        //Profile Photos
-        const maleProfilePhoto = `https://kidneystoneindia.com/wp-content/uploads/2018/05/dummy-profile-pic-male1-270x270?=${username}.jpg`;
-
-        const femaleProfilePhoto = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRumd-Z9wdBnnDYHzvGBxOBwNQu7CZzjiewBw&s?=${username}.jpg`;
-
-        await User.create({
-            fullName,
-            username,
-            password: hashedPassword,
-            profilePhoto,
-            gender
-        })
-    } catch (error) {
-        console.log(error);
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ message: "All fields are required." });
     }
-}
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match." });
+    }
+
+    const user = await User.findOne({ username });
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "Username already exist. Try different!" });
+    }
+
+    // Password hashed to maintain security.
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //Profile Photos
+    const maleProfilePhoto = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_XcZeOyQUF35CuG7kddgs3HFhiTa5I0wDBQ&s?=${username}.jpg`;
+
+    const femaleProfilePhoto = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRumd-Z9wdBnnDYHzvGBxOBwNQu7CZzjiewBw&s?=${username}.jpg`;
+
+    await User.create({
+      fullName,
+      username,
+      password: hashedPassword,
+      profilePhoto: gender === "male" ? maleProfilePhoto : femaleProfilePhoto,
+      gender,
+    });
+
+    return res.status(201).json({
+        message: "Account created successfully.",
+        success:true
+    })
+  } catch (error) {
+    console.log(error);
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,24 @@ dotenv.config({});
 
 import connectDB from "./config/database.js";
 
+connectDB();
+
+import userRoute from "./routes/userRoute.js"
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+//middleware
+app.use(express.json())
+
+//routes
+app.get("/", (req, res) => {
+  res.send("🚀 Backend is running");
+});
+
+app.use("/api/v1/user", userRoute)
+
 app.listen(PORT, ()=>{
-    connectDB();
     console.log(`Server listen at port: ${PORT}`);
 })
