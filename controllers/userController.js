0922044368f7c0f281d33b596f8b6aa997623582
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-03-26 11:00:39 +0530

    updt

--- controllers/userController.js
+++ controllers/userController.js
@@ -84,7 +84,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
       })
       .json({
         _id: user._id,
