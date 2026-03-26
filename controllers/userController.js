e8131f6c252d6371047a4ddd60ea388887721900
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-03-26 13:02:24 +0530

    secured

--- controllers/userController.js
+++ controllers/userController.js
@@ -85,6 +85,7 @@ export const login = async (req, res) => {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "none",
+        secure: true,
       })
       .json({
         _id: user._id,
