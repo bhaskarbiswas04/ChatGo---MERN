c907e75bcca5fbcb36c80328d24c00e54a83e97f
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-05-07 21:51:20 +0530

    updt: send token to fe

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -2,17 +2,20 @@ import jwt from "jsonwebtoken";
 const isAuthenticated = async (req, res, next) => {
   try {
 
-   const token = req.cookies.token;
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(" ")[1];
     
     if (!token) {
       return res.status(401).json({ message: "User not authenticated." });
     }
+
     const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY);
     if (!decode) {
       return res.status(401).json({ message: "Invalid token" });
     }
     req.id = decode.userId;
     next();
+    
   } catch (error) {
     console.log(error);
   }
